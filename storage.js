4679c7d2418b3a34b90d0a0b9e0ad41da6e8c6e6
Dacian FIORDEAN <dacian.fiordean@younited-credit.fr>
2024-11-07 14:35:38 +0100

    fix: use blobClient `downloadToFile` for file downloads and simplify promise handling; use `done` callback for asynchronous tests; clean `_workdata` test directory.

--- storage.js
+++ storage.js
@@ -9,195 +9,129 @@ const renderDir = _config.renderPath || path.join(__dirname, '..', 'render');
 let blobServiceClient;
 
 if (_config?.storageCredentials) {
-  const sharedKeyCredential = new StorageSharedKeyCredential(
-    _config.storageCredentials.accountName,
-    _config.storageCredentials.accountKey
-  );
-  const blobServiceClientOptions = {
-    credential: sharedKeyCredential,
-    url: `https://${_config.storageCredentials.accountName}.blob.core.windows.net`
-  };
-  blobServiceClient = new BlobServiceClient(
-    blobServiceClientOptions.url,
-    blobServiceClientOptions.credential);
+    const sharedKeyCredential = new StorageSharedKeyCredential(
+        _config.storageCredentials.accountName,
+        _config.storageCredentials.accountKey
+    );
+    const blobServiceClientOptions = {
+        credential: sharedKeyCredential,
+        url: `https://${_config.storageCredentials.accountName}.blob.core.windows.net`
+    };
+    blobServiceClient = new BlobServiceClient(
+        blobServiceClientOptions.url,
+        blobServiceClientOptions.credential);
 }
 
 function writeTemplate(req, res, templateId, templatePath, callback) {
-  const headers = {};
+    const headers = {};
 
-  if (!_config?.templatesContainer) {
-    return callback(null, templateId);
-  }
-
-  headers['content-type'] = req.headers?.['carbone-template-mimetype'] ?? 'application/octet-stream';
-
-  const containerClient = blobServiceClient.getContainerClient(_config.templatesContainer);
-  const blockBlobClient = containerClient.getBlockBlobClient(templateId);
-
-  fs.readFile(templatePath, (err, data) => {
-    if (err) {
-      return callback(err);
-    }
-    return blockBlobClient.upload(data, data.length, { blobHTTPHeaders: { blobContentType: headers['content-type'] } })
-      .then(response => {
-        if (response._response.status !== 201) {
-          return callback(new Error(`Status: ${response._response.status} | Body: ${response.errorCode ?? response._response.bodyAsText}`));
-        }
+    if (!_config?.templatesContainer) {
         return callback(null, templateId);
-      })
-      .catch(err => {
-        return callback(err, templateId);
-      });
-  });
-}
-
-function readTemplate(req, res, templateId, callback) {
-  const templatePath = path.join(templateDir, templateId);
-
-  if (!_config?.templatesContainer) {
-    return callback(null, templatePath);
-  }
-
-  fs.access(templatePath, fs.F_OK, (err) => {
-    if (err) {
-      const containerClient = blobServiceClient.getContainerClient(_config.templatesContainer);
-      const blockBlobClient = containerClient.getBlockBlobClient(templateId);
-      const downloadOptions = { abortSignal: AbortController?.signal };
-
-      return blockBlobClient.download(0, undefined, downloadOptions)
-        .then(response => {
-          if (response._response.status === 404) {
-            return callback(new Error('File does not exist'));
-          }
-          if (response._response.status !== 200) {
-            return callback(new Error(`Status: ${response._response.status} | Body: ${response.errorCode ?? response._response.bodyAsText}`));
-          }
-
-          const stream = response.readableStreamBody;
-          const writableStream = fs.createWriteStream(templatePath);
-          stream.pipe(writableStream);
-          writableStream.on('error', (err) => {
-            return callback(err)
-          });
-          writableStream.on('finish', () => {
-            return callback(null, templatePath);
-          });
-        })
-        .catch(err => {
-          return callback(err);
-        });
     }
-    return callback(null, templatePath);
-  });
-}
 
-function deleteTemplate(req, res, templateId, callback) {
-  const templatePath = path.join(templateDir, templateId);
+    headers['content-type'] = req.headers?.['carbone-template-mimetype'] ?? 'application/octet-stream';
 
-  if (!_config?.templatesContainer) {
-    return callback(null, templatePath);
-  }
+    const containerClient = blobServiceClient.getContainerClient(_config.templatesContainer);
+    const blockBlobClient = containerClient.getBlockBlobClient(templateId);
 
-  const containerClient = blobServiceClient.getContainerClient(_config.templatesContainer);
-  const blockBlobClient = containerClient.getBlockBlobClient(templateId);
-
-  return blockBlobClient.delete()
-    .then(response => {
-      if (response._response.status !== 202) {
-        return callback(new Error(`Status: ${response._response.status} | Body: ${response.errorCode ?? response._response.bodyAsText}`));
-      }
-      return callback(null, templatePath);
-    })
-    .catch(err => {
-      return callback(err);
+    fs.readFile(templatePath, (err, data) => {
+        if (err) {
+            return callback(err);
+        }
+        return blockBlobClient.upload(data, data.length, { blobHTTPHeaders: { blobContentType: headers['content-type'] } })
+            .then(_ => callback(null, templateId))
+            .catch(err => callback(err, templateId));
     });
 }
 
-function readRender(req, res, renderId, callback) {
-  const renderPath = path.join(renderDir, renderId);
+function readTemplate(req, res, templateId, callback) {
+    const templatePath = path.join(templateDir, templateId);
 
-  if (!_config.rendersContainer) {
-    return callback(null, renderPath);
-  }
-
-  const containerClient = blobServiceClient.getContainerClient(_config.rendersContainer);
-  const blobClient = containerClient.getBlobClient(renderId);
-
-  fs.access(renderPath, fs.constants.F_OK, (err) => {
-    if (err) {
-      return blobClient.download(0)
-        .then(response => {
-          if (response._response.status === 404) {
-            return callback(new Error('File does not exist'))
-          }
-          if (response._response.status !== 200) {
-            return callback(new Error(`Status: ${response._response.status} | Body: ${response.errorCode ?? response._response.bodyAsText}`))
-          }
-
-          const stream = response.readableStreamBody;
-          const writableStream = fs.createWriteStream(renderPath);
-          stream.pipe(writableStream);
-          writableStream.on('error', (err) => {
-            return callback(err)
-          });
-          writableStream.on('finish', () => {
-            blobClient.delete()
-              .then(() => {
-                return callback(null, renderPath)
-              })
-              .catch(err => {
-                return callback(err)
-              });
-          });
-        })
-        .catch(err => {
-          return callback(new Error(`Error downloading blob: ${err.message}`));
-        });
-    } 
-    else {
-      return blobClient.delete()
-        .then(() => {
-          return callback(null, renderPath)
-        })
-        .catch(err => {
-          return callback(err)
-        });
+    if (!_config?.templatesContainer) {
+        return callback(null, templatePath);
     }
-  });
+
+    fs.access(templatePath, fs.constants.R_OK, (err) => {
+        if (err) {
+            const containerClient = blobServiceClient.getContainerClient(_config.templatesContainer);
+            const blockBlobClient = containerClient.getBlockBlobClient(templateId);
+            const downloadOptions = { abortSignal: AbortController?.signal };
+
+            return blockBlobClient.downloadToFile(templatePath, 0, undefined, downloadOptions)
+                .then(_ => callback(null, templatePath))
+                .catch(err => callback(err));
+        }
+        return callback(null, templatePath);
+    });
+}
+
+function deleteTemplate(req, res, templateId, callback) {
+    const templatePath = path.join(templateDir, templateId);
+
+    if (!_config?.templatesContainer) {
+        return callback(null, templatePath);
+    }
+
+    const containerClient = blobServiceClient.getContainerClient(_config.templatesContainer);
+    const blockBlobClient = containerClient.getBlockBlobClient(templateId);
+
+    return blockBlobClient.delete()
+        .then(_ => callback(null, templatePath))
+        .catch(err => callback(err));
+}
+
+function readRender(req, res, renderId, callback) {
+    const renderPath = path.join(renderDir, renderId);
+
+    if (!_config?.rendersContainer) {
+        return callback(null, renderPath);
+    }
+
+    const containerClient = blobServiceClient.getContainerClient(_config.rendersContainer);
+    const blobClient = containerClient.getBlobClient(renderId);
+    const downloadOptions = { abortSignal: AbortController?.signal };
+
+    fs.access(renderPath, fs.constants.R_OK, (err) => {
+        if (err) {
+            return blobClient.downloadToFile(renderPath, 0, undefined, downloadOptions)
+                .then(_ => blobClient.delete())
+                .then(_ => callback(null, renderPath))
+                .catch(err => callback(err));
+        }
+        else {
+            return blobClient.delete()
+                .then(_ => callback(null, renderPath))
+                .catch(err => callback(err));
+        }
+    });
 }
 
 function afterRender(req, res, err, reportPath, reportName, stats, callback) {
-  if (err) {
-    return callback(err);
-  }
-  if (!_config?.rendersContainer) {
-    return callback();
-  }
-
-  const containerClient = blobServiceClient.getContainerClient(_config.rendersContainer);
-  const blockBlobClient = containerClient.getBlockBlobClient(reportName);
-
-  fs.readFile(reportPath, (err, data) => {
     if (err) {
-      return callback(err);
-    }
-    return blockBlobClient.upload(data, data.length, { blobHTTPHeaders: { blobContentType: 'application/octet-stream' } })
-      .then(response => {
-        if (response._response.status !== 201) {
-          return callback(new Error(`Status: ${response._response.status} | Body: ${response.errorCode ?? response._response.bodyAsText}`));
-        }
-        return callback();
-      })
-      .catch(err => {
         return callback(err);
-      });
-  });
+    }
+    if (!_config?.rendersContainer) {
+        return callback();
+    }
+
+    const filename = reportName && reportName?.length > 0 ? reportName : path.basename(reportPath);
+    const containerClient = blobServiceClient.getContainerClient(_config.rendersContainer);
+    const blockBlobClient = containerClient.getBlockBlobClient(filename);
+
+    fs.readFile(reportPath, (err, data) => {
+        if (err) {
+            return callback(err);
+        }
+        return blockBlobClient.upload(data, data.length, { blobHTTPHeaders: { blobContentType: 'application/octet-stream' } })
+            .then(_ => callback())
+            .catch(err => callback(err));
+    });
 }
 
 module.exports = {
-  writeTemplate,
-  readTemplate,
-  deleteTemplate,
-  readRender,
-  afterRender
+    writeTemplate,
+    readTemplate,
+    deleteTemplate,
+    readRender,
+    afterRender
 };
